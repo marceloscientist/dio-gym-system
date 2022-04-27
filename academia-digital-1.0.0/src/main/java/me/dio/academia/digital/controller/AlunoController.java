8058bc841505a101c6448f26a3916783b0c83c15
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2022-04-26 23:30:31 -0300

    Started adding the first implementation of the Pessoa(Person) CRUD

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,29 @@
 package me.dio.academia.digital.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.service.impl.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoService alunoService;
+
+    @GetMapping
+    public
+    List<Aluno> getAll() {
+        return alunoService.getAll();
+    }
+
+    @PostMapping
+    public
+    void create(@RequestBody AlunoForm form) {
+        alunoService.create(form);
+    }
 }
